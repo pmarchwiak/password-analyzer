154f4d1ba8b2434c2e9f01f417a90e0f6945e530
Patrick Marchwiak <pd@marchwiak.com>
2011-10-26 01:24:44 -0700

    Got basic length analysis and results display working.  Added jquery.

--- data/start.js
+++ data/start.js
@@ -1,8 +1,18 @@
 var analyzeButton = document.getElementById("analyze-button");
 console.log("Got analyze button " + analyzeButton);
 
+self.port.on("report", function(report) {
+  console.log("Received report:" + report);
+  var tbody = $("#report-table");
+  for (var i = 0; i < report.length; i++) {
+    entry = report[i];
+    console.log("Appending " + entry.toSource() + " to report");
+    tbody.append("<tr><td>" + entry.url + "</td><td>" + entry.password.length
+      + "</td></tr>");
+  }
+});
+
 analyzeButton.onclick = function() {
   console.log("Clicked analyzer passwords");
   self.port.emit("analyze", {option1: false});
 };
-
