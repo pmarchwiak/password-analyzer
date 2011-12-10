c0d3bf99a8ad55f3f4a1ecddaa9793a510a71667
Patrick Marchwiak <pd@marchwiak.com>
2011-12-10 15:46:16 -0800

    Show message when no problems found.

--- data/panel.js
+++ data/panel.js
@@ -8,54 +8,60 @@ var tableContainer = document.getElementById("table-container");
 self.port.on("report", function(report) {
   console.log("Received report:" + report);
   
-  //TODO hide this in the static html rather than adding it to the dom
-  var tableHtml = '<table><tr><th>URL</th><th>Problems <a href="#info">(more info)</a></th><th>Username</th>' +
-    '<th>Password</th><th><input type="checkbox" id="check-all" /></th></tr>';
-  
-  for (var n = 0; n < report.all.length; n++) {
-    var entry = report.all[n];
-    console.log("Appending " + entry.toSource() + " to report");
+  if (report.all.length == 0 || report.all[0].score == 0) {
+    tableContainer.innerHTML += "<span>No problems found.</span>";
+  }
+  else {
+    //TODO hide this in the static html rather than adding it to the dom
+    var tableHtml = '<table><tr><th>URL</th>' + 
+      '<th>Problems <a href="#info">(more info)</a></th><th>Username</th>' +
+      '<th>Password</th><th><input type="checkbox" id="check-all" /></th></tr>';
     
-    // TODO consider using a templating language instead of this ugliness
-    tableHtml += '<tr><td>' + 
-      '<a href="' + entry.formSubmitURL + '" target="_blank">' + 
-      entry.url + '</a></td>';
-    tableHtml += '<td><ul>';
-    for (var i = 0; i < entry.issues.length; i++) {
-      tableHtml += '<li>' + entry.issues[i] + '</li>';
+    for (var n = 0; n < report.all.length; n++) {
+      var entry = report.all[n];
+      console.log("Appending " + entry.toSource() + " to report");
+      
+      // TODO consider using a templating language instead of this ugliness
+      tableHtml += '<tr><td>' + 
+        '<a href="' + entry.formSubmitURL + '" target="_blank">' + 
+        entry.url + '</a></td>';
+      tableHtml += '<td><ul>';
+      for (var i = 0; i < entry.issues.length; i++) {
+        tableHtml += '<li>' + entry.issues[i] + '</li>';
+      }
+      tableHtml += '</ul></td>';
+      tableHtml += '<td>' + entry.username + '</td>';
+      tableHtml += '<td class="password">';
+      tableHtml += '<input class="password-field" type="password"';
+      tableHtml += ' disabled="disabled" value="' + entry.password + '" /></td>';
+      // enable checkbox if "bad"
+      var checked = entry.score > 0 ? 'checked="on"' : "";
+      tableHtml += '<td><input name="tab-checkbox"' +
+       ' value="' + entry.formSubmitURL + '" type="checkbox"' +
+       checked + ' ></td></tr>';
     }
-    tableHtml += '</ul></td>';
-    tableHtml += '<td>' + entry.username + '</td>';
-    tableHtml += '<td class="password">';
-    tableHtml += '<input class="password-field" type="password"';
-    tableHtml += ' disabled="disabled" value="' + entry.password + '" /></td>';
-    // enable checkbox if "bad"
-    var checked = entry.score > 0 ? 'checked="on"' : "";
-    tableHtml += '<td><input name="tab-checkbox"' +
-     ' value="' + entry.formSubmitURL + '" type="checkbox"' +
-     checked + ' ></td></tr>';
-  }
-  tableHtml += '</table>';
-  tableContainer.innerHTML += tableHtml;
-  
-  var hiddens = document.getElementsByClassName("hide");
-  for (var i = 0; i < hiddens.length; i++) {
-    console.log("Showing hidden " + hiddens[i].id);
-    hiddens[i].style.visibility = "visible";
-  }
-  
-  var checkAll = document.getElementById("check-all");
-  checkAll.onchange = function() {
-    console.log("Clicked check all");
-    var checkboxes = document.getElementsByName("tab-checkbox");
-    for (var i = 0; i < checkboxes.length; i++) {
-      var field = checkboxes[i];
-      if (checkAll.checked) {
-        field.checked = true;
+    tableHtml += '</table>';
+    tableContainer.innerHTML += tableHtml;
+    
+     var checkAll = document.getElementById("check-all");
+    checkAll.onchange = function() {
+      console.log("Clicked check all");
+      var checkboxes = document.getElementsByName("tab-checkbox");
+      for (var i = 0; i < checkboxes.length; i++) {
+        var field = checkboxes[i];
+        if (checkAll.checked) {
+          field.checked = true;
+        }
+        else {
+          field.checked = false;
+        }
       }
-      else {
-        field.checked = false;
-      }
+    }
+    
+    var hiddens = document.getElementsByClassName("hide");
+    for (var i = 0; i < hiddens.length; i++) {
+      console.log("Showing hidden " + hiddens[i].id);
+      hiddens[i].style.visibility = "visible";
     }
   }
   
